297087220aef9cca5e33fc412a419a68265ee410
Rifqi <rifqi@kontinentalist.com>
2024-01-09 12:01:04 +0700

    fix: Change router history to using hash for legacy access

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import AutoImport from "unplugin-auto-import/vite";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
