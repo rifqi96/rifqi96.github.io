59e920e93e299d8be4a42f6357dad73d0c4c2786
Rifqi <rifqi@kontinentalist.com>
2025-02-10 03:00:20 +0700

    chore: Update default slot value

--- public/margin-calc-legacy/main.js
+++ public/margin-calc-legacy/main.js
@@ -93,7 +93,7 @@ function getAuthInfo() {
   const slotInput = document.getElementById("slot");
   const apiSecretInput = document.getElementById("apiSecret");
 
-  slot = slotInput.value || "15";
+  slot = slotInput.value || "16";
   apiSecret =
     apiSecretInput.value ||
     "3fa7c1ec483bcc7112ccf94552194fe21576d5b8259f49891ef6e5a5aaia2419";
