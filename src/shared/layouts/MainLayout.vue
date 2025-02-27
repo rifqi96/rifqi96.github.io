3db470283ce04bcde1da471e1241061c03776837
Rifqi <rifqi@kontinentalist.com>
2025-02-27 22:00:06 +0700

    fix(Loading): Add better coloring

--- src/shared/layouts/MainLayout.vue
+++ src/shared/layouts/MainLayout.vue
@@ -282,8 +282,8 @@ onMounted(() => {
   top: 0;
   left: 0;
   width: 100%;
-  height: 3px;
-  background-color: rgba(200, 200, 200, 0.2);
+  height: 6px;
+  background-color: rgba(50, 50, 50, 0.5);
   overflow: hidden;
   z-index: 2000;
 }
